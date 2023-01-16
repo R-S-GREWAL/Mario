35fef186094c7e73ae6422364e1a5167e65f5034
R-S-GREWAL <88488978+R-S-GREWAL@users.noreply.github.com>
2023-01-16 19:28:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kill = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_gameover = loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+function gotPoses()
+{
+	if(results.length>0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+function modelLoaded()
+{
+	console.log('Model Loaded !');
+}
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(900,325);
+	video.parent('game_console');
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+	game();
+}
+
+
